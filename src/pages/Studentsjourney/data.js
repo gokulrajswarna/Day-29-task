53b12c5bfa03394a128784aba3650fa1c597c3e6
gokulrajswarna <123186805+gokulrajswarna@users.noreply.github.com>
2023-10-27 20:59:34 +0530

    Add files via upload

---
+++ src/pages/Studentsjourney/data.js
@@ -0,0 +1,16 @@
+const Array = [
+    { id:"1",
+      name:"Gokul",
+      age:30,
+      email:"gokul@gmail.com"
+},
+{
+      id:"2",
+      name:"Raj",
+      age:32,
+      email:"raj@gmail.com"
+    }
+    
+    
+]
+export default Array;
